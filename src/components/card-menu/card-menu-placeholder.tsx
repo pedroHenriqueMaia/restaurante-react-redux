4a3f3dce64d637e0d7d26712803a3461f6c02560
Pedro Henrique Maia <phmaia2018@gmail.com>
2024-06-20 02:27:08 -0300

    FIX: Ajusa bugs de lint para build

--- src/components/card-menu/card-menu-placeholder.tsx
+++ src/components/card-menu/card-menu-placeholder.tsx
@@ -7,29 +7,26 @@ const CardMenuPlaceholder = () => {
     <div className="container">
       <div className="row">
         {loop.map((_, index) => (
-          <div key={index} className="col-12 col-sm-6 col-md-4 mb-3 mt-3">
-            <div className="card h-100">
+          <div key={index} className="col-4 mb-3 mt-3">
+            <div className="card" style={{ width: '25rem', height: '18rem' }} aria-hidden="true">
               <div className="card-body">
                 <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-6">Placeholder Text</span> {/* Exemplo de texto dentro do âncora */}
                 </h5>
                 <p className="card-text placeholder-glow">
-                  <span className="placeholder col-7"></span>
-                  <span className="placeholder col-4"></span>
-                  <span className="placeholder col-4"></span>
-                  <span className="placeholder col-6"></span>
-                  <span className="placeholder col-8"></span>
+                  <span className="placeholder col-7">Placeholder Text</span>
+                  <span className="placeholder col-4">Placeholder Text</span>
+                  <span className="placeholder col-4">Placeholder Text</span>
+                  <span className="placeholder col-6">Placeholder Text</span>
+                  <span className="placeholder col-8">Placeholder Text</span>
                 </p>
                 <div className="row mt-5">
-                  <span className="placeholder col-5"></span>
-                  <span className="placeholder col-5"></span>
+                  <span className="placeholder col-5">Placeholder Text</span>
+                  <span className="placeholder col-5">Placeholder Text</span>
                 </div>
               </div>
               <div className="card-footer">
-                <a
-                  className="btn disabled placeholder col-6"
-                  aria-disabled="true"
-                ></a>
+                <button className="btn disabled placeholder col-6" aria-disabled="true">Placeholder Button</button>
               </div>
             </div>
           </div>
